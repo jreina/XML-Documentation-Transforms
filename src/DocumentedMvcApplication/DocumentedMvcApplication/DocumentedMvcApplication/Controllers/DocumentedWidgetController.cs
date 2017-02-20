546f0d83c1dc8733c4cd3007b17a05331b8cea6a
Johnny Reina <johnny.r.reina@gmail.com>
2017-02-19 22:44:23 -0600

    Adds an MVC app.

---
+++ src/DocumentedMvcApplication/DocumentedMvcApplication/DocumentedMvcApplication/Controllers/DocumentedWidgetController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocumentedMvcApplication.Controllers
+{
+    /// <summary>
+    /// This is the controller for the Documented Widget.
+    /// 
+    /// This widget does some things to the database on the back end.
+    /// 
+    /// On the front end, this widget has some very specific requirements in the CMS, here they are:
+    /// 1. Requirement 1
+    /// 2. Requirement 2
+    /// 3. Requirement 3
+    /// 4. Requirement 4
+    /// </summary>
+    public class DocumentedWidgetController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/DocumentedMvcApplication/DocumentedMvcApplication/DocumentedMvcApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DocumentedMvcApplication.MvcApplication" Language="C#" %>
