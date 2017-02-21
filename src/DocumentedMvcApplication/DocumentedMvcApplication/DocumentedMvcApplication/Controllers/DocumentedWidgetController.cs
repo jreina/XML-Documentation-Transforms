f4060c8f234fc784e64aca949c6f2f6868967c61
Johnny Reina <johnny.r.reina@gmail.com>
2017-02-20 21:50:59 -0600

    Updates GFM documentation for DocumentedMVCApplication.

--- src/DocumentedMvcApplication/DocumentedMvcApplication/DocumentedMvcApplication/Controllers/DocumentedWidgetController.cs
+++ src/DocumentedMvcApplication/DocumentedMvcApplication/DocumentedMvcApplication/Controllers/DocumentedWidgetController.cs
@@ -12,11 +12,15 @@ namespace DocumentedMvcApplication.Controllers
     /// This widget does some things to the database on the back end.
     /// 
     /// On the front end, this widget has some very specific requirements in the CMS, here they are:
-    /// 1. Requirement 1
-    /// 2. Requirement 2
-    /// 3. Requirement 3
-    /// 4. Requirement 4
-    /// </summary>
+    /// 
+    /// 1. Requirement 1  
+    /// 
+    /// 2. Requirement 2  
+    /// 
+    /// 3. Requirement 3  
+    /// 
+    /// 4. Requirement 4  
+    /// </summary> 
     public class DocumentedWidgetController : Controller
     {
         public ActionResult Index()
